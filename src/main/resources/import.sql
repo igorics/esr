c485cc941c8cf91386c6b8e2bed477c99dbefbc1
igorics <igorcandido@gmail.com>
2019-10-15 02:21:57 -0300

    Commit fechando capitulo 3 do curso.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Cia do XIS', '5', 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Besten Burguer', '15', 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Compania da Pizza', '20', 2);
