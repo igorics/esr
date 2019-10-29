61dfac2f3b83e8b79b6a3ae293cfbd7b0c2016d6
igorics <igorcandido@gmail.com>
2019-10-29 02:55:14 -0300

    Atualizações do Cap. 4

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,6 @@ insert into cozinha (nome) values ('Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Cia do XIS', '5', 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Besten Burguer', '15', 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Compania da Pizza', '20', 2);
+
+insert into estado (nome) values ('MS');
+insert into estado (nome) values ('RS');
