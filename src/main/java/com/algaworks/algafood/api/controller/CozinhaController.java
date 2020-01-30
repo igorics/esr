c5994a972aa6e0ec9b8e9e69f68f99221f047185
igorics <igorcandido@gmail.com>
2020-01-30 00:13:27 -0300

    Exemplo de mudança de status na chamada do serviço.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,8 +34,22 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
+	// @ResponseStatus(HttpStatus.CREATED) Exemplo da alteração do código de retorno do serviço (200,2001,400,etc)
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable("cozinhaId") Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		// return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		// return ResponseEntity.ok(cozinha);// Esta linha é um atalho para a linha acima
+		
+		/**
+		 * Exemplo de status 302
+		 */
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		return ResponseEntity
+						.status(HttpStatus.FOUND)
+						.headers(headers)
+						.build();
 	}
+	
 }
